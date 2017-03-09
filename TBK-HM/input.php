9f06a976f5b8e5fe6e43c9c31df3642de8d882e0
shima <shima@server.local>
2017-03-09 23:47:11 +0900

    凡ミス修正…＞ ＜

--- TBK-HM/input.php
+++ TBK-HM/input.php
@@ -49,19 +49,19 @@ $_SESSION['menu'] = 1;
   </form>
 </div>
 <script>
-$("#isbn13").on("focusout", function {
+$("#isbn13").on("focusout", function(){
   var tmp = checkCommon.escapeHtml($("#isbn13").val());
   $("#isbn13").val(tmp);
 });
-$("#isbn10").on("focusout", function {
+$("#isbn10").on("focusout", function(){
   var tmp = checkCommon.escapeHtml($("#isbn10").val());
   $("#isbn10").val(tmp);
 });
-$("#bookname").on("focusout", function {
+$("#bookname").on("focusout", function(){
   var tmp = checkCommon.escapeHtml($("#bookname").val());
   $("#bookname").val(tmp);
 });
-$("#author").on("focusout", function {
+$("#author").on("focusout", function(){
   var tmp = checkCommon.escapeHtml($("#author").val());
   $("#author").val(tmp);
 });
